5fa17f24c7e478b35ec073aaa12b6d426b4ad1e4
Artemy <artemyk@gmail.com>
2019-08-09 14:47:44 -0600

    Local push

--- code/mnist_demo.py
+++ code/mnist_demo.py
@@ -20,14 +20,11 @@ savedirbase  = str(pathlib.Path().absolute()) + '/saveddata-scipy/'
 
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.9, beta2=0.999) 
 
-runtype = 'MNIST'
-
+n_batch          = 128     # SGD batch size
+n_noisevar_batch = 1000    # batch size for training noise variance when train_noisevar='optimizer'
 cfg = {
-    'runtype'         : runtype,      # which dataset we're running
-    'n_batch'         : 128 ,         # SGD batch size
     'train_noisevar'  : 'gradient',   # train noise variance with gradient descent ('gradient'), 
                                       #  an external optimizer loop ('optimizer'), or leave fixed ('none')
-    'n_noisevar_batch': 1000,         # batch size for training noise variance when train_noisevar='optimizer'
     'initial_fitvar'  : False,        # whether to set noisevar to optimal value before training
     'squaredIB'       : False,        # optimize I(Y;T)-beta*I(X;T) or I(Y;T)-beta*I(X;T)^2 
     'err_func'        : 'softmax_ce', # 'softmax_ce' for classification, 'mse' for regression  
@@ -55,90 +52,79 @@ betavals = 10**np.linspace(-5, 0.1, 30, endpoint=True)
 
 
 
-def get_net():
-
-
 for beta in betavals:
-    if np.isclose(beta,0): 
-        continue
-    for mode in ['nlIB','VIB',]:
-        tf.reset_default_graph()
-        with tf.Session() as sess:
+    tf.reset_default_graph()
+    with tf.Session() as sess:
+        n = model.Net(input_dims   = data['trn_X'].shape[1],
+                      encoder_arch = cfg['encoder_arch'], 
+                      decoder_arch = cfg['decoder_arch'],
+                      err_func     = cfg['err_func'],
+                      entropyY     = data['entropyY'],
+                      trainable_noisevar = cfg['train_noisevar']=='gradient', 
+                      noisevar     = 0.01,
+                      kdewidth    = -20.)            
 
-            
-            n = model.Net(input_dims   = data['trn_X'].shape[1],
-                          encoder_arch = cfg['encoder_arch'], 
-                          decoder_arch = cfg['decoder_arch'],
-                          err_func     = cfg['err_func'],
-                          entropyY     = data['entropyY'],
-                          trainable_noisevar = cfg['train_noisevar']=='gradient', 
-                          noisevar     = 0.01,
-                          kdewidth    = -20.)            
-            
-            saver = tf.train.Saver(max_to_keep=30) # save last 30 epochs
-            saver.restore(sess, tf.train.latest_checkpoint(savedir+'/basemodel'))
-            sqmode  = 'sq'  if cfg['squaredIB'] else 'reg'
-            print("Doing %s, beta=%0.4f, %s" % (mode, beta, sqmode))
-            
-            n_batch          = cfg['n_batch']
-            n_noisevar_batch = cfg['n_noisevar_batch']
-            saved_data       = []
-            n_mini_batches   = int(len(data['trn_Y']) / n_batch)
+        sess.run(tf.global_variables_initializer())
 
-            Ixt  = n.Ixt if mode == 'nlIB' else n.vIxt
-            loss = beta*(Ixt**2 if cfg['squaredIB'] else Ixt) - n.Iyt
+        #saver = tf.train.Saver(max_to_keep=30) # save last 30 epochs
+        #saver.restore(sess, tf.train.latest_checkpoint(savedir+'/basemodel'))
+        sqmode  = 'sq'  if cfg['squaredIB'] else 'reg'
+        print("Doing beta=%0.4f, %s" % (beta, sqmode))
 
-            trainstep  = optimizer.minimize(loss)
-            sess.run(tf.variables_initializer(optimizer.variables()))
+        saved_data       = []
+        n_mini_batches   = int(len(data['trn_Y']) / n_batch)
 
-            #cdata = stats(sess, mode, beta, loss, 0, data, n, do_print=True)
+        Ixt  = n.Ixt # TODO if mode == 'nlIB' else n.vIxt
+        loss = beta*(Ixt**2 if cfg['squaredIB'] else Ixt) - n.Iyt
 
-            noisevar_optimizer = None
+        trainstep  = optimizer.minimize(loss)
+        sess.run(tf.variables_initializer(optimizer.variables()))
 
-            start_time = time.time()
-            for epoch in range(cfg['n_epochs']):
-                # randomize order of training data
-                permutation  = np.random.permutation(len(data['trn_Y']))
-                train_X = data['trn_X'][permutation]
-                train_Y = data['trn_Y'][permutation]
+        #cdata = stats(sess, mode, beta, loss, 0, data, n, do_print=True)
 
-                x_batch, y_batch, dmatrix = None, None, None
+        noisevar_optimizer = None
 
-                train_kdewidth = cfg['train_kdewidth']
-                train_noisevar = (fit_var and epoch == 0) or (cfg['train_noisevar']=='optimizer') #  and epoch % 30 == 0)
+        start_time = time.time()
+        for epoch in range(cfg['n_epochs']):
+            # randomize order of training data
+            permutation  = np.random.permutation(len(data['trn_Y']))
+            train_X = data['trn_X'][permutation]
+            train_Y = data['trn_Y'][permutation]
 
-                if train_kdewidth or train_noisevar:
-                    x_batch = train_X[:n_noisevar_batch]
-                    y_batch = train_Y[:n_noisevar_batch]
-                    #dmatrix = sess.run(n.distance_matrix, feed_dict={n.x: x_batch})
+            x_batch, y_batch, dmatrix = None, None, None
 
-                if train_kdewidth: # Set kernel width
-                    n.eta_optimizer.minimize(sess, feed_dict={n.x: x_batch}) # feed_dict={n.distance_matrix: dmatrix})
+            train_kdewidth = cfg['train_kdewidth']
+            train_noisevar = (cfg['train_noisevar']=='optimizer')
 
-                if train_noisevar: # Set noise variance with an external optimizer
-                    if noisevar_optimizer is None:
-                        noisevar_optimizer = n.get_noisevar_optimizer(loss)
-                    noisevar_optimizer.minimize(sess, feed_dict={n.x: x_batch, n.y: y_batch})# , n.distance_matrix: dmatrix})
+            if train_kdewidth or train_noisevar:
+                x_batch = train_X[:n_noisevar_batch]
+                y_batch = train_Y[:n_noisevar_batch]
+                #dmatrix = sess.run(n.distance_matrix, feed_dict={n.x: x_batch})
 
-                for batch in range(n_mini_batches):
-                    # sample mini-batch
-                    x_batch = train_X[batch * n_batch:(1 + batch) * n_batch]
-                    y_batch = train_Y[batch * n_batch:(1 + batch) * n_batch]
-                    cparams = {n.x: x_batch, n.y: y_batch}
-                    sess.run(trainstep, feed_dict=cparams)
+            if train_kdewidth: # Set kernel width
+                n.eta_optimizer.minimize(sess, feed_dict={n.x: x_batch}) # feed_dict={n.distance_matrix: dmatrix})
 
-                #cdata = stats(sess, mode, beta, loss, epoch+1, data, n, start_time=start_time, do_print=epoch % report_every == 0)
-                #saved_data.append(cdata)
-                #write_data(savedir, epoch+1, sess, saver, [cfg, saved_data])
+            if train_noisevar: # Set noise variance with an external optimizer
+                if noisevar_optimizer is None:
+                    noisevar_optimizer = n.get_noisevar_optimizer(loss)
+                noisevar_optimizer.minimize(sess, feed_dict={n.x: x_batch, n.y: y_batch})# , n.distance_matrix: dmatrix})
+
+            for batch in range(n_mini_batches):
+                # sample mini-batch
+                x_batch = train_X[batch * n_batch:(1 + batch) * n_batch]
+                y_batch = train_Y[batch * n_batch:(1 + batch) * n_batch]
+                cparams = {n.x: x_batch, n.y: y_batch}
+                sess.run(trainstep, feed_dict=cparams)
+
+            #cdata = stats(sess, mode, beta, loss, epoch+1, data, n, start_time=start_time, do_print=epoch % report_every == 0)
+            #saved_data.append(cdata)
+            #write_data(savedir, epoch+1, sess, saver, [cfg, saved_data])
 
 #             trainutils.train(sess, saver, mode, beta, cfg, data, n, optimizer, report_every=report_every, 
 #                              savedir=savedir + '/results-%s-%0.5f-%s-run%d' % (mode, beta, sqmode, runndx), 
 #                              fit_var=cfg['initial_fitvar'])
-            del saver
+        del saver
 
-            print()
-            print()
-
-
-
+        print()
+        print()
 
